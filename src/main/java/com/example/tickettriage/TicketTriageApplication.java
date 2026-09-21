5d5765f7c118bfb9a122e2859114ad9dd8c917df
anthonycheng <chengan222+github@gmail.com>
2026-09-21 14:02:48 -0500

    Spring app

---
+++ src/main/java/com/example/tickettriage/TicketTriageApplication.java
@@ -0,0 +1,12 @@
+package com.example.tickettriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTriageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketTriageApplication.class, args);
+    }
+}
